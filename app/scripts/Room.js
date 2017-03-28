1f80085fd2d102619a776cec4f1a8632cf39e620
megansawkar <megan.sawkar@gmail.com>
2017-03-27 17:14:00 -0700

    Checkpoint 3: Update

--- app/scripts/Room.js
+++ app/scripts/Room.js
@@ -1,15 +1,16 @@
 (function() {
 		function Room($firebaseArray) {
+				var Room = {};
+			
 				var ref = firebase.database().ref().child("rooms");
 				var rooms = $firebaseArray(ref);
 			
-				return {
-      			all: rooms,
-					  addRooom: function(roomName) {
-							rooms.$add({name: roomName});
-						}
-    		};
-}
+				Room.all = rooms;
+				Room.add = function(roomName) {
+							rooms.$add(roomName);
+				};
+				return Room;
+    }
 
 angular
 		.module('blocChat')
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,6 @@
 		}
 	
 		angular 
-				.module('blocChat', ['ui.router', 'firebase'])
+				.module('blocChat', ['ui.router', 'ui.bootstrap', 'firebase'])
 				.config(config);
 })();
